c03a39b704f0b579f9021f0354ef78752e4b1d3a
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-03 14:39:36 +0500

    initiate project

---
+++ app/index.jsx
@@ -0,0 +1,21 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aura</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color:'blue' }}>Go to profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
